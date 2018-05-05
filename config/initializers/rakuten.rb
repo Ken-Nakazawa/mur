0c4399cc2c58a3c5ab1ab2214987cbd2514b0aee
中澤 健 <KenNakazawa@nakazawa-no-Air.lan>
2018-05-05 18:43:43 +0900

    0505

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,6 +1,6 @@
 RakutenWebService.configuration do |c|
-    c.application_id = '1013927750706173361'
-    c.affiliate_id = '16a1078a.5776ab5e.16a1078b.4f3c0e15'#任意
+    c.application_id = ''
+    c.affiliate_id = ''#任意
 
     c.max_retries = 5 # default: 5
     c.debug = true # default: false

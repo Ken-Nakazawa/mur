27b4c00c021910773f6d24d41dce7002c6b20c45
中澤 健 <KenNakazawa@nakazawa-no-Air.lan>
2018-05-21 21:38:44 +0900

    0521

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,13 +16,14 @@ class PagesController < ApplicationController
     end
 
     @tweets = []
+
     since_id = nil
     # 検索ワードが存在していたらツイートを取得
     if params[:keyword].present?
-      # リツイートを除く、検索ワードにひっかかった最新20件のツイートを取得する
-      tweets = client.search(params[:keyword], count: 20, result_type: "recent", exclude: "retweets", since_id: since_id)
+      # リツイートを除く、検索ワードにひっかかった最新30件のツイートを取得する
+      tweets = client.search(params[:keyword], count: 30, result_type: "recent", exclude: "retweets", since_id: since_id)
       # 取得したツイートをモデルに渡す
-      tweets.take(20).each do |tw|
+      tweets.take(30).each do |tw|
         tweet = Tweet.new(tw.full_text)
         @tweets << tweet
       end
